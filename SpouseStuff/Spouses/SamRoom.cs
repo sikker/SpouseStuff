49230c092a70910aa4f68675a5b4a9aab6b530c7
Per Sikker Hansen <persikkerhansen@gmail.com>
2019-03-02 20:12:48 +0100

    Add project files.

---
+++ SpouseStuff/Spouses/SamRoom.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewModdingAPI.Utilities;
+using StardewValley;
+using StardewValley.Minigames;
+using SpouseStuff.Spouses;
+using SpouseStuff;
+
+namespace SpouseStuff.Spouses
+{
+    class SamRoom : ISpouseRoom
+    {
+        public void InteractWithSpot(int tileX, int tileY, int faceDirection)
+        {
+
+        }
+    }
+}
